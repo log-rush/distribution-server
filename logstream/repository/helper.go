4b5cef7b8d872f051179203dc186324bef01434c
fabiankachlock <fabian.kachlock@icloud.com>
2022-06-12 22:40:06 +0200

    fix: removing item from slice tahts not in the slice

--- logstream/repository/helper.go
+++ logstream/repository/helper.go
@@ -17,12 +17,15 @@ func AppendUniqueToSlice[T comparable](slc *[]T, v T) *[]T {
 }
 
 func RemoveFromSlice[T comparable](slc *[]T, v T) *[]T {
-	var index int
+	index := -1
 	for i, item := range *slc {
 		if item == v {
 			index = i
 			break
 		}
+	}
+	if index == -1 {
+		return slc
 	}
 	newSlice := append((*slc)[:index], (*slc)[index+1:]...)
 	return &newSlice
--- subscriptions/repository/helper.go
+++ subscriptions/repository/helper.go
@@ -11,12 +11,15 @@ func AppendUniqueToSlice[T any](slc *[]T, v T, comp func(v T) bool) *[]T {
 }
 
 func RemoveFromSlice[T any](slc *[]T, comp func(v T) bool) *[]T {
-	var index int
+	index := -1
 	for i, item := range *slc {
 		if comp(item) {
 			index = i
 			break
 		}
+	}
+	if index == -1 {
+		return slc
 	}
 	newSlice := append((*slc)[:index], (*slc)[index+1:]...)
 	return &newSlice
