d759c5b0e948e69400a43798c2c9a37a1d293453
fabiankachlock <fabian.kachlock@icloud.com>
2022-09-02 18:00:23 +0200

    fix: logging plain string in mockLogger

--- pkg/devkit/logger.go
+++ pkg/devkit/logger.go
@@ -24,42 +24,42 @@ func NewLogger(handler func(level LogLevel, log string, args ...interface{})) do
 }
 
 func (l *mockLogger) Debug(args ...interface{}) {
-	l.handler(LogLevelDebug, l.prefix, args...)
+	l.handler(LogLevelDebug, l.prefix+"%s", args...)
 }
 func (l *mockLogger) Debugf(template string, args ...interface{}) {
 	l.handler(LogLevelDebug, l.prefix+template, args...)
 }
 
 func (l *mockLogger) Error(args ...interface{}) {
-	l.handler(LogLevelError, l.prefix, args...)
+	l.handler(LogLevelError, l.prefix+"%s", args...)
 }
 func (l *mockLogger) Errorf(template string, args ...interface{}) {
 	l.handler(LogLevelError, l.prefix+template, args...)
 }
 
 func (l *mockLogger) Fatal(args ...interface{}) {
-	l.handler(LogLevelFatal, l.prefix, args...)
+	l.handler(LogLevelFatal, l.prefix+"%s", args...)
 }
 func (l *mockLogger) Fatalf(template string, args ...interface{}) {
 	l.handler(LogLevelFatal, l.prefix+template, args...)
 }
 
 func (l *mockLogger) Info(args ...interface{}) {
-	l.handler(LogLevelInfo, l.prefix, args...)
+	l.handler(LogLevelInfo, l.prefix+"%s", args...)
 }
 func (l *mockLogger) Infof(template string, args ...interface{}) {
 	l.handler(LogLevelInfo, l.prefix+template, args...)
 }
 
 func (l *mockLogger) Panic(args ...interface{}) {
-	l.handler(LogLevelPanic, l.prefix, args...)
+	l.handler(LogLevelPanic, l.prefix+"%s", args...)
 }
 func (l *mockLogger) Panicf(template string, args ...interface{}) {
 	l.handler(LogLevelPanic, l.prefix+template, args...)
 }
 
 func (l *mockLogger) Warn(args ...interface{}) {
-	l.handler(LogLevelWarn, l.prefix, args...)
+	l.handler(LogLevelWarn, l.prefix+"%s", args...)
 }
 func (l *mockLogger) Warnf(template string, args ...interface{}) {
 	l.handler(LogLevelWarn, l.prefix+template, args...)
